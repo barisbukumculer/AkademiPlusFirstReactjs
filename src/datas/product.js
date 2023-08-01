132fb1d08c83ee8314208f611734cb3913a976a7
Barış Bükümcüler <barisbukumculer@gmail.com>
2023-08-01 23:22:27 +0300

    akademi plus frontend

---
+++ src/datas/product.js
@@ -0,0 +1,9 @@
+export const arr = [
+    {image:"https://picsum.photos/640/360?random=1",title:"TV",detail:"TV Detail"},
+    {image:"https://picsum.photos/640/360?random=2",title:"iPhone",detail:"iPhone Detail"},
+    {image:"https://picsum.photos/640/360?random=3",title:"Kitap",detail:"Kitap Detail"},
+    {image:"https://picsum.photos/640/360?random=4",title:"Bilgisayar",detail:"Bilgisayar Detail"},
+    {image:"https://picsum.photos/640/360?random=5",title:"Oyun",detail:"Oyun Detail"}
+  ]
+
+export const appTitle="Başlık"
